6f1916c8c1d30b27d2e5a8836dcbbc1783c6e10a
Eddie Otieno Ratego <eddieratego@gmail.com>
2025-09-30 17:57:28 +0300

    Add RPS backed functionality

--- rock.js
+++ rock.js
@@ -1,84 +1,83 @@
-const options=["paper","rock","scissors"];
-//declaring a function that get computer choice
-function getComputerChoice() {
- const random=options[Math.floor(Math.random()*options.length)];
- return random;
-
+ const options=["PAPER","ROCK","SCISSORS"];
+         //declaring a function that get computer choice
+        function getComputerChoice() {
+            const random=options[Math.floor(Math.random()*options.length)];
+            return random;
 }
-getComputerChoice();
+        let playerScore=0;
+        let compScore=0;
+//getComputerChoice();
+        document.addEventListener("DOMContentLoaded",function(){
+            const Btn=document.getElementById("choiceBtn");
+            const compDisplay=document.getElementById("computerChoice");
+            const humanDisplay=document.getElementById("yourChoice");
+            const butonRock=document.getElementById("rockBtn");
+            const butonPaper=document.getElementById("paperBtn");
+            const butonScissors=document.getElementById("scissorsBtn");
+            const resultMessage=document.getElementById("result");
+            const myResult=document.getElementById('yourResult');
+            const compResult=document.getElementById("computerResult");
 
-function getHumanChoice() {
-    let validateInput=false;
-    while (validateInput==false) {
-         const prompt = require('prompt-sync')();
-         const humanChoice=prompt("Enter your choice?(rock or paper or scissors)");
-         if (humanChoice==null) {
-            continue;  
-         }
-        let humanChoiceInLower=humanChoice.toLowerCase();
-        if (options.includes(humanChoiceInLower)) {
-            validateInput=true;
-            return humanChoiceInLower;   
-    }         
-    }    
-}
-//console.log(getHumanChoice());
-//declaring a function to play a sigle round
-function playRound(humanSelection,computerChoice) {
-    const computer=getComputerChoice();
-    const human=getHumanChoice();
-   // const targetValue="ROCK";
-    if (human.toLocaleLowerCase()===computer.toLowerCase()) {
-        return "its a draw"
-    }else if (computer.toLowerCase()=="rock"&&human.toLocaleLowerCase()=="scissors") {
-            return "Rock smashes scissors you lose!"
-            
-        } else if (computer.toLowerCase()=="scissors"&&human.toLocaleLowerCase()==="rock") {
-            return "you win scissors beats rock"
-            
-        } else if (computer.toLowerCase()=="scissors"&&human.toLocaleLowerCase()=="paper") {
-            return "You lose scissors cuts paper"
-            
-        } else if (computer.toLowerCase()=="paper"&&human.toLocaleLowerCase()=="scissors") {
-            return "You win paper beats scissor"
-            
-        } else if (computer.toLowerCase()=="paper"&&human.toLocaleLowerCase()=="rock") {
-            return "You lose paper covers rock"
-            
-        } else if (computer.toLowerCase()=="rock"&&human.toLocaleLowerCase()=="paper") {
-            return "You win paper covers rock"
-            
-        } 
-        
+            function playground(playchoice){
+            const compChoice=getComputerChoice();
+
+            //display choices
+            compDisplay.textContent=compChoice
+            humanDisplay.textContent=playchoice;
+
+            //DETERMINE THE WINNER
+            let winner='';
+
+            //winner selection
+            if(playchoice==compChoice){
+                resultMessage.textContent="ITS A DRAW";
+                winner="draw";
+            }else if(playchoice==="SCISSORS"&&compChoice==="ROCK"){
+                resultMessage.textContent="YOU LOSE! Rock smashes scissors";
+                winner="computer"
+            }else if(playchoice==="ROCK"&&compChoice==="SCISSORS"){
+                resultMessage.textContent="You WIN! rock smashes scissors";
+                winner="player"
+            }else if(playchoice==="PAPER"&&compChoice==="SCISSORS"){
+                resultMessage.textContent="You LOSE! scissors cuts paper";
+                winner="computer"
+            }else if(playchoice==="SCISSORS"&&compChoice==="PAPER"){
+                resultMessage.textContent="You WIN scissors cuts paper"
+                winner="player"
+            }else if(playchoice==="ROCK"&&compChoice==="PAPER"){
+                resultMessage.textContent="You LOSE! Paper covers rock";
+                winner="computer"
+            }else if(playchoice==="PAPER"&&compChoice==="ROCK"){
+                resultMessage.textContent="You WIN! Paper covers rock"
+                winner="player"
+            }   
+            return winner;
+        }
+
+        function updateScore(winner){
+           if(winner==="player"){
+            playerScore++
+            myResult.textContent=playerScore;
+           }else if(winner==="computer"){
+            compScore++;
+            compResult.textContent=compScore;
+           } 
     }
-const humanSelection=getHumanChoice();
-const computerChoice=getComputerChoice();
-console.log(playRound(humanSelection,computerChoice));
 
-function game() {
-    let scorePlayer=0;
-    let computerScore=0;
-    for (let i = 0; i < 5; i++) {
-        const humanSelection=getHumanChoice();
-        const computerChoice=getComputerChoice();
-        console.log(getComputerChoice());
-        console.log("player "+scorePlayer+" vs computer "+computerScore);
-        if ((playRound(humanSelection,computerChoice)===("You win paper beats scissor"))||(playRound(humanSelection,computerChoice)===("You win paper covers rock"))||(playRound(humanSelection,computerChoice)===("you win scissors beats rock"))) {
-            scorePlayer++;
-            
-        }else if ((playRound(humanSelection,computerChoice)===("You lose paper covers rock"))||(playRound(humanSelection,computerChoice)===("You lose scissors cuts paper"))||(playRound(humanSelection,computerChoice)===("Rock smashes scissors you lose!"))) {
-            computerScore++;  
-        } 
-    }  
-    console.log("Game over");
-    if (scorePlayer>computerScore) {
-            console.log("Congrats you win");
-        
-         }else if(scorePlayer<computerScore){
-            console.log("You lose better try your luck next time");
-         }else{
-            console.log("Its a tire");
-         }    
-}
-console.log(game());
+           
 
+           butonRock.addEventListener("click",function(){
+            const winner=playground("ROCK");  
+            updateScore(winner);
+           });
+
+           butonPaper.addEventListener("click",function(){
+            const winner=playground("PAPER");
+            updateScore(winner);
+           });
+
+           butonScissors.addEventListener("click",function(){
+            const winner=playground("SCISSORS");
+            updateScore(winner);
+           });
+        })
